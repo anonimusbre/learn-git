eb7836e09fe5476f6aa25c58fa37fb94272b9bb0
M <anonimusbre@gmail.com>
2015-03-30 17:34:43 +0200

    Initial Import

---
+++ src/main/java/com/learn/learninggit/DummyClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.learn.learninggit;
+
+/**
+ *
+ * @author dexter
+ */
+public class DummyClass {
+
+    public DummyClass() {
+        System.out.println("Print From Dummy Class");
+    }
+    
+}
